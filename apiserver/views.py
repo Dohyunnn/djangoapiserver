2c610104b491cb7a9b8d6f3e12c833decb805d9c
Dohyunnn <92616533+Dohyunnn@users.noreply.github.com>
2025-07-25 15:17:34 +0900

    Update views.py

--- apiserver/views.py
+++ apiserver/views.py
@@ -4,6 +4,5 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result":"success", "data":[{"id":"itstudy","name":"군계"},
-                                        {"id":"ggangppae1", "name":"아담"}]}
+    data = {"result":"success", "data":[{"id":"dohyeon","name":"도현"}
     return Response(data, status=status.HTTP_200_OK)
